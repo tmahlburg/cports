51fdc77093f888f88ffd6fd0691d28f33b050a97
Till Mahlburg <mahlburg@posteo.de>
2025-08-01 22:57:53 +0200

    user/libfm-qt: new package

--- user/libfm-qt/template.py
+++ user/libfm-qt/template.py
@@ -21,7 +21,6 @@ license = "LGPL-2.1-or-later"
 url = "https://github.com/lxqt/libfm-qt"
 source = f"https://github.com/lxqt/libfm-qt/releases/download/{pkgver}/libfm-qt-{pkgver}.tar.xz"
 sha256 = "4d8aa86fcfcf424f7f41c4a931e8d804dd12bedc8428931b5bc955345c4313a9"
-hardening = ["vis", "cfi"]
 
 @subpackage("libfm-qt-devel")
 def _(self):
